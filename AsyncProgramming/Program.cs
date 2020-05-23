1134acc133641d48ead351a0db70d4ac201ab638
emanuele <emanuele.sabbadini@gmail.com>
2020-05-23 09:12:14 +0200

    end course

--- AsyncProgramming/Program.cs
+++ AsyncProgramming/Program.cs
@@ -10,11 +10,14 @@ using System.Threading.Tasks;
 
 namespace AsyncProgramming
 {
+
+
+
     public class Program
     {
 
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
 
 
